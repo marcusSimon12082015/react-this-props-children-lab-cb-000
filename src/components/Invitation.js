5ff39c834f82f8249c5921b377a0cb10f8d9ccd3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 08:12:14 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-export default class Invitation extends React.Component 
+export default class Invitation extends React.Component
 {
-  
+
   render(){
     return();
   }
