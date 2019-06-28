834b86804c297bc84b1eef8e8362f03979515060
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 08:15:21 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -8,6 +8,7 @@ export default class Invitation extends React.Component
     return(<div>
         <h1>{You've been invited!}</h1>
         <div>{this.props.children}</div>
-      </div>);
+      </div>
+    );
   }
 }
